d8efb647a5dc9ae1434f363a73235cc8fb45b552
Mr-K86 <112010529+Mr-K86@users.noreply.github.com>
2025-04-04 14:35:25 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
+// Write a Java program to create a class called Vehicle with a method called drive().
+// Create a subclass called Car that overrides the drive() method to print "Repairing a car".
 // Base class
+
 class Vehicle {
     // Method to be overridden
     public void drive() {
